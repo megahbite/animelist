f4a67c35aa07498497a25e2c30bd3cd8a551fd82
Megan Bowra-Dean <Megan Bowra-Dean>
2017-04-23 17:42:51 +1200

    Fix ordering

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @front_page = FrontPageAnimesDecorator.decorate(Anime.order(rank: :asc).page params[:page])
+    @front_page = FrontPageAnimesDecorator.decorate(Anime.order(latest_rank: :asc).page params[:page])
   end
 end

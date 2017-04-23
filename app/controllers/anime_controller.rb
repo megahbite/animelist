4c1b8a493ff9cfc7fa8e827618758138ef846202
Megan Bowra-Dean <Megan Bowra-Dean>
2017-04-23 21:03:46 +1200

    Fix bug with decorated object

--- app/controllers/anime_controller.rb
+++ app/controllers/anime_controller.rb
@@ -1,6 +1,7 @@
 class AnimeController < ApplicationController
+  decorates_assigned :anime
   def show
-    @anime = Anime.find(params[:id]).decorate
+    @anime = Anime.find(params[:id])
     @user_scores = UserScore.where(anime: @anime).order(score: :desc, status: :asc, watched: :desc, reddit_name: :asc).decorate
   end
 end

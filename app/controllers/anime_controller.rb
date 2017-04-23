668dbaf08d3d8d0ad4ababdd15bce6fe93303cac
Megan Bowra-Dean <Megan Bowra-Dean>
2017-04-23 21:00:13 +1200

    User scores on anime page.

--- app/controllers/anime_controller.rb
+++ app/controllers/anime_controller.rb
@@ -1,5 +1,6 @@
 class AnimeController < ApplicationController
   def show
     @anime = Anime.find(params[:id]).decorate
+    @user_scores = UserScore.where(anime: @anime).order(score: :desc, status: :asc, watched: :desc, :reddit_name).decorate
   end
 end
---
+++ app/decorators/user_score_decorator.rb
@@ -0,0 +1,30 @@
+class UserScoreDecorator < Draper::Decorator
+  delegate :watched
+
+  def reddit_name
+    object.user.reddit_name
+  end
+
+  def mal_name
+    object.user.mal_name
+  end
+
+  def score
+    object.score > 0 ? object.score : ""
+  end
+
+  def status
+    case object.status
+    when 1
+      "Watching"
+    when 2
+      "Completed"
+    when 3
+      "On Hold"
+    when 4
+      "Dropped"
+    when 6
+      "Plan to Watch"
+    end
+  end
+end

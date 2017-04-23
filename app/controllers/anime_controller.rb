594129b90d13a269ab522892461bf888c61de281
Megan Bowra-Dean <Megan Bowra-Dean>
2017-04-23 21:01:26 +1200

    Ordering

--- app/controllers/anime_controller.rb
+++ app/controllers/anime_controller.rb
@@ -1,6 +1,6 @@
 class AnimeController < ApplicationController
   def show
     @anime = Anime.find(params[:id]).decorate
-    @user_scores = UserScore.where(anime: @anime).order(score: :desc, status: :asc, watched: :desc, :reddit_name).decorate
+    @user_scores = UserScore.where(anime: @anime).order(score: :desc, status: :asc, watched: :desc, reddit_name: :asc).decorate
   end
 end

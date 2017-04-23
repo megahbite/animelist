14f1e12588fccf4cc38cf222993fe3ef02f1bbca
Megan Bowra-Dean <Megan Bowra-Dean>
2017-04-23 21:05:48 +1200

    Syntax problem

--- app/controllers/anime_controller.rb
+++ app/controllers/anime_controller.rb
@@ -2,6 +2,6 @@ class AnimeController < ApplicationController
   decorates_assigned :anime
   def show
     @anime = Anime.find(params[:id])
-    @user_scores = UserScore.joins(:user).where(anime: @anime).order(score: :desc, status: :asc, watched: :desc, users: { reddit_name: :asc }).decorate
+    @user_scores = UserScore.joins(:user).where(anime: @anime).order(score: :desc, status: :asc, watched: :desc, "users.reddit_name" => :asc }).decorate
   end
 end

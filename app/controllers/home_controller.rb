1bb66eed25899f732e2829ca64e591102b4da323
Megan Bowra-Dean <meganbd@showgizmo.com>
2017-04-24 22:41:14 +1200

    Sorting.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,28 @@
 
 class HomeController < ApplicationController
   def index
-    @front_page = FrontPageAnimesDecorator.decorate(Anime.order(latest_rank: :asc).page(params[:page]))
+    @front_page = FrontPageAnimesDecorator.decorate(anime)
+  end
+
+  private
+
+  def anime
+    sort(Anime.where.not(latest_score: 0, latest_mal_score: nil)).page(params[:page])
+  end
+
+  def sort(anime_list)
+    sort_method = params[:sort]
+    sort_direction = "desc".eql?(params[:direction].try(:downcase)) ? "desc" : "asc"
+
+    case sort_method
+    when "mal"
+      anime_list.order(latest_mal_rank: sort_direction)
+    when "difference"
+      anime_list.order("(latest_score - latest_mal_score) #{sort_direction}")
+    when "title"
+      anime_list.order(title: sort_direction)
+    else
+      anime_list.order(latest_rank: sort_direction)
+    end
   end
 end

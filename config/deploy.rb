18eead08f2eede3680eb167d305542d04c8d13cd
Megan Bowra-Dean <Megan Bowra-Dean>
2017-05-06 20:13:23 +1200

    Capistrano config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid only for current version of Capistrano
 lock "3.8.1"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "animelist-rails"
+set :repo_url, "https://github.com/megahbite/animelist.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/srv/animelist-rails"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -21,13 +21,18 @@ set :repo_url, "git@example.com:me/my_repo.git"
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_files, "config/database.yml", "config/secrets.yml", "config/app_config.yml"
 
 # Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+
+set :puma_workers, 2
+set :puma_env, :production
+
+set :conditionally_migrate, true
